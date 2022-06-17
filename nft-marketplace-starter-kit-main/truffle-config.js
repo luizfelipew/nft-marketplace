52e87c526fc6a5aec13c968b6eaa3494b217eb5f
Luiz Felipe Wendt Campos <luiz.campos@picpay.com>
2022-06-17 16:45:08 -0300

    configurações finais para compilar os contratos

--- nft-marketplace-starter-kit-main/truffle-config.js
+++ nft-marketplace-starter-kit-main/truffle-config.js
@@ -1,22 +1,21 @@
 module.exports = {
 
   networks: {
-  
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-     port: 7545,
-     network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1",     // Localhost (default: none)
+      //  port: 8545,            // Standard Ethereum port (default: none)
+      port: 7545,
+      network_id: "*",       // Any network (default: none)
     },
-
-    contracts_directory: './src/contracts',
-    contracts_build_directory: './src/abis',
   },
+
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis',
 
   // Configure your compilers
   compilers: {
     solc: {
-      version: "^0.8.14", 
+      version: "^0.8.14",
       optimizer: {
         enable: true,
         runs: 200
